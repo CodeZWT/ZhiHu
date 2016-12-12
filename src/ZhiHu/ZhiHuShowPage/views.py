1c2a8ebdae479e42ee6923df767f562a495321ba
zuoakang <944528536@qq.com>
2016-12-12 16:44:04 +0800

    给主页添加样式
    同时修改了登入，添加了form

--- src/ZhiHu/ZhiHuShowPage/views.py
+++ src/ZhiHu/ZhiHuShowPage/views.py
@@ -5,9 +5,9 @@ from ZhiHuShowPage.models import Question
 #跳转到主页面
 def index(reqest):
     
-    questions = Question.objects.all()[0:10]
-    return render_to_response("index.html", {'questions':questions})
-
+    findResult = find(reqest, Question)
+    return render_to_response("index.html", {'questions':findResult['datas'], 'allPage':findResult["allPage"], 'curPage':findResult["curPage"]})
+    
 #跳转到登入页面
 def login(reqest):
     
@@ -17,3 +17,39 @@ def login(reqest):
 def register(reqest):
     
     return render_to_response("register.html")
+
+def find(reqest, dbName):
+    
+    ONE_PAGE_OF_DATA = 15 
+    result = {}
+    try:  
+        curPage = int(reqest.GET.get('curPage', '1'))  
+        allPage = int(reqest.GET.get('allPage', '1'))  
+        pageType = str(reqest.GET.get('pageType', ''))  
+    except ValueError:  
+        curPage = 1  
+        allPage = 1  
+        pageType = ''  
+  
+    #判断点击了【下一页】还是【上一页】  
+    if pageType == 'pageDown':  
+        curPage += 1  
+    elif pageType == 'pageUp':  
+        curPage -= 1  
+    elif pageType == 'lastDown':
+        curPage = allPage
+    startPos = (curPage - 1) * ONE_PAGE_OF_DATA  
+    endPos = startPos + ONE_PAGE_OF_DATA  
+    datas = dbName.objects.all()[startPos:endPos]  
+  
+    if curPage == 1 and allPage == 1: #标记1  
+        allPostCounts = dbName.objects.count()  
+        allPage = allPostCounts / ONE_PAGE_OF_DATA  
+        remainPost = allPostCounts % ONE_PAGE_OF_DATA  
+        if remainPost > 0:  
+            allPage += 1 
+    
+    result["datas"] = datas
+    result["allPage"] = allPage
+    result["curPage"] = curPage
+    return result
