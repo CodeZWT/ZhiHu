6cdbb84f730193e97921adfda4b2b6a3f25dbb55
CodeZWT <1561280060@qq.com>
2016-12-13 22:01:16 +0800

    Add 个人中心界面,views2

---
+++ src/ZhiHu/ZhiHuShowPage/views2.py
@@ -0,0 +1,25 @@
+#coding=UTF-8
+'''
+Created on Dec 13, 2016
+
+@author: ZWT
+'''
+#coding:utf-8
+from django.shortcuts import render, render_to_response
+from django.contrib.messages.storage import session
+from ZhiHuShowPage.models import Question,Person,Topicfollow,TopicIdIntroduction
+
+
+
+#跳转至用户个人中心
+def person(request):
+    User = request.session.get("loginUser", "none")
+    tempperson = Person.objects.get(id = User.id)
+    followTopic_list = Topicfollow.objects.filter(id_p = tempperson.id)
+    topic_list = []
+    for topic in followTopic_list:
+        id = topic.topicid
+        topicname = TopicIdIntroduction.objects.get(topicid=id)
+        topic_list.append(topicname)
+    topic_list = set(topic_list)
+    return render_to_response("person.html",{'person':tempperson,'followTopic':topic_list})
