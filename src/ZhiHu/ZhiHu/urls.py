b4ebfd2bdf50f5b2756f1e9b34f4a10daa306b9b
CodeZWT <1561280060@qq.com>
2016-12-13 21:56:22 +0800

    Add 个人中心界面demo,views2

--- src/ZhiHu/ZhiHu/urls.py
+++ src/ZhiHu/ZhiHu/urls.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 from django.conf.urls import url
 from django.contrib import admin
-from ZhiHuShowPage import views
+from ZhiHuShowPage import views,views2
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^register/', views.register, name = 'register'),
     url(r'^topic/$', views.topic, name = 'topic'),
     url(r'^logout/', views.logout, name = 'logout'),
+    
+    url(r'^person/$',views2.person,name = 'person'),
 ]
